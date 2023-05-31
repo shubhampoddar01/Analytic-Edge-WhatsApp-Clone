3e1636494cf971c46413e35f458056d43adcb21b
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-05-31 20:00:23 +0530

    Whatsapp clone app

---
+++ src/Data.js
@@ -0,0 +1,75 @@
+export const contactList = [
+    {
+      id: 1,
+      name: "+91 90080 15668",
+      profilePic: "/profile/pp1.png",
+      lastText: "ok",
+      lastTextTime: "12:58 PM",
+    },
+    {
+      id: 2,
+      name: "+91 88964 77007",
+      profilePic: "/profile/pp2.png",
+      lastText: `ill share the mail with uh`,
+      lastTextTime: "12:45 PM",
+    },
+    {
+      id: 3,
+      name: "Priyanshu",
+      profilePic: "/profile/pp3.jpeg",
+      lastText: "baba lets go out?",
+      lastTextTime: "12:30 PM",
+    },
+    {
+      id: 4,
+      name: "Kaushal",
+      profilePic: "/profile/pp4.jpeg",
+      lastText: "no broo",
+      lastTextTime: "12:00 PM",
+    },
+  ];
+  export const messagesList = [
+    {
+      id: 1,
+      messageType: "TEXT",
+      text: "Hello",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 2,
+      messageType: "TEXT",
+      text: "What's up?",
+      senderID: 1,
+      addedOn: "12:01 PM",
+    },
+    {
+      id: 3,
+      messageType: "TEXT",
+      text: "All Good, What about you?",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 4,
+      messageType: "TEXT",
+      text: "I'm good as well",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 5,
+      messageType: "TEXT",
+      text: "Great 😁",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 6,
+      messageType: "TEXT",
+      text: "ok",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+    
+  ];
